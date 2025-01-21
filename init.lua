13b397c481e4a7823ca7a99edf1d3f42047c01ae
topper3418 <travisopperud@gmail.com>
2025-01-21 01:52:09 -0600

    added frontend languages

--- init.lua
+++ init.lua
@@ -835,7 +835,23 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'vim',
+        'vimdoc',
+        'typescript',
+        'javascript',
+        'python',
+        'go',
+        'json',
+        'css',
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
@@ -883,6 +899,8 @@ require('lazy').setup({
   require 'custom.plugins.oil',
   -- lazy git, for git stuff
   require 'custom.plugins.lazygit',
+  -- github copilot
+  require 'custom.plugins.copilot',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
