f6d4fe654c0ab8196a2385864cc6ba5e7e2325cc
topper3418 <travisopperud@gmail.com>
2025-01-31 21:43:10 -0600

    line numbers in all their glory

--- init.lua
+++ init.lua
@@ -951,5 +951,8 @@ vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'TelescopeBorder', { bg = 'none' })
 
+vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#51B3EC', bold = true })
+vim.api.nvim_set_hl(0, 'LineNr', { fg = 'white', bold = true })
+vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#FB508F', bold = true })
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
